7b0ff2992950087ae235f7ced3a3883217319aa2
missionsave <foundationmissionsave@gmail.com>
2019-07-31 21:48:35 +0100

    update

--- site/index.php
+++ site/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<?php $indexp="indexr3.php"; $indexp=""; ?>
+<?php $indexp="index1.php"; $indexp=""; ?>
 <html>
 
 <head>
@@ -11,7 +11,7 @@
 <meta http-equiv="pragma" content="no-cache" />
 <link rel="shortcut icon" href="icon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title>Mission Save</title>
+
 <?php
 session_set_cookie_params(3600000,"/");
 session_start(); 
@@ -24,7 +24,9 @@ $lg=0;
 $dlg = array("pt", "en");
 
 $dini = array("Inicio", "Home");
+$prot = array("Protótipo", "Prototype");
 $ufp = array("Unidade de produção alimentar", "Unit of food production");
+$orc = array("Orçamento", "Budget");
 $jobs = array("Trabalho", "Jobs");
 $strat = array("Estratégia", "Strategy");
 $contact = array("Contacto", "Contact");
@@ -52,7 +54,7 @@ $lg=$_GET["l"];
 
 
 if(@$_GET["pg"]=="") {
-	$_GET["pg"]="home_"; 	
+	$_GET["pg"]="home"; 	
 }
 ?>
 <script>
@@ -62,7 +64,18 @@ document.write(event.data);
 }
 </script>
 
+<?php 	
+	if( $_GET["pg"]=="fund")$stitle=$fund[$lg]; 
+	if( $_GET["pg"]=="plan")$stitle=$plan[$lg]; 
+	if( $_GET["pg"]=="donate")$stitle=$doar[$lg];
+	if( $_GET["pg"]=="estrategia")$stitle=$strat[$lg];
+	if( $_GET["pg"]=="jobs")$stitle=$jobs[$lg];
+	if( $_GET["pg"]=="descritiva")$stitle=$ufp[$lg]; 
+	if( $_GET["pg"]=="develop")$stitle=$dev[$lg];  
+	if( $_GET["pg"]=="budget")$stitle=$orc[$lg];       
+?>
 
+<title>Better Planet Mission <?php echo @$stitle; ?></title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 
@@ -70,6 +83,7 @@ document.write(event.data);
 .topnav {  
   overflow: hidden;
   background-color: #333; 
+  background-image: linear-gradient(gray, #333);
 }
 
 .topnav a {
@@ -190,12 +204,10 @@ body {
  
 .htr {    
     max-width: 21cm;
-margin-right: 1em;
-    display: table;
 	margin:10px auto;
 	font-family:Arial; 
     color:black;
-    text-align:justify;
+    text-align:left;
 	padding-left:10px;
 	padding-right:10px;
 	    
@@ -211,26 +223,40 @@ margin-right: 1em;
 	  display: block; 
 	}
 }
+
+ 	
 </style>
 </head>
-<body>		
-<div class="topheader">
+<body  onresize="onresizeFunction()">		
+<div class="topheader" id="topheaderid">
 <div class="topnav" id="myTopnav">
-	<a href="	<?php echo  $indexp.'?pg=home_'.'&l='.$lg;	?>" class="active">Mission Save</a>
+	<a href="	<?php echo  $indexp.'?pg=home'.'&l='.$lg;	?>" class="active">Better Planet Mission</a>
   
-	<a href="	<?php echo  $indexp.'?pg=descritiva_'.'&l='.$lg;	?>  "><?php echo $ufp[$lg];?></a>
+    <div class="dropdown">
+    <button onclick="dropdownFunction('myDropdown1')" class="dropbtn"><?php echo $prot[$lg];?>
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div  id="myDropdown1" class="dropdown-content">
+	<a href="	<?php echo  $indexp.'?pg=descritiva'.'&l='.$lg;	?>  "><?php echo $ufp[$lg];?></a>
+	<a href="	<?php echo  $indexp.'?pg=budget'.'&l='.$lg;	?>  "><?php echo $orc[$lg];?></a>
+    </div>
+  </div>
   
   
-	<a href="	<?php echo  $indexp.'?pg=estrategia_'.'&l='.$lg;	?>  "><?php echo $strat[$lg];?></a>
-	<a href="	<?php echo  $indexp.'?pg=jobs_'.'&l='.$lg;	?>  "><?php echo $jobs[$lg];?></a>
-	<a href="	<?php echo  $indexp.'?pg=donate_'.'&l='.$lg;	?>  "><?php echo $doar[$lg];?></a>
-	<a href="	<?php echo  $indexp.'?pg=develop_'.'&l='.$lg;	?>  "><?php echo $dev[$lg];?></a> 
+   
+	<a href="	<?php echo  $indexp.'?pg=jobs'.'&l='.$lg;	?>  "><?php echo $jobs[$lg];?></a>
+	<a href="	<?php echo  $indexp.'?pg=donate'.'&l='.$lg;	?>  "><?php echo $doar[$lg];?></a>
+	<a href="	<?php echo  $indexp.'?pg=develop'.'&l='.$lg;	?>  "><?php echo $dev[$lg];?></a> 
   
   
-	<a href="	<?php echo  $indexp.'?pg=plan_'.'&l='.$lg;	?>  "><?php echo $plan[$lg];?></a> 
-<!--
-	<a href="	<?php echo  $indexp.'?pg=fund'.'&l='.$lg;	?>  "><?php echo $fund[$lg];?></a> 
--->  
+	<a href="	<?php echo  $indexp.'?pg=plan'.'&l='.$lg;	?>  "><?php echo $plan[$lg];?></a> 
+ 
+<!--		<a href="	<?php echo  $indexp.'?pg=fund'.'&l='.$lg;	?>  "><?php echo $fund[$lg];?></a>  -->
+	
+	<a href="	<?php echo  $indexp.'?pg=faq'.'&l='.$lg;	?>  "><?php echo 'FAQ';?></a> 
+	
+<!--	<a href="	<?php echo  $indexp.'?pg=nutri'.'&l='.$lg;	?>  "><?php echo 'Sobre';?></a> -->
+ 
 	
 	<a href="	<?php echo  $indexp.'?pg='.$_GET['pg'].'&l='.($lg==1?'0':'1') ;	?>  "><?php echo $idiom[$lg];?></a> 
 
@@ -252,48 +278,75 @@ margin-right: 1em;
 </div>
 </div>
 
-<div   align="center"  style="padding-top:75px; padding-left:10px; padding-right:10px;">
-<?php 	
-	if( $_GET["pg"]=="fund")echo '<h2>'.$fund[$lg].'</h2>'; 
-	if( $_GET["pg"]=="plan_")echo '<h2>'.$plan[$lg].'</h2>'; 
-	if( $_GET["pg"]=="donate_")echo '<h2>'.$doar[$lg].'</h2>';
-	if( $_GET["pg"]=="estrategia_")echo '<h2>'.$strat[$lg].'</h2>';
-	if( $_GET["pg"]=="jobs_")echo '<h2>'.$jobs[$lg].'</h2>';
-	if( $_GET["pg"]=="descritiva_")echo '<h2>'.$ufp[$lg].'</h2>'; 
-	if( $_GET["pg"]=="develop_")echo '<h2>'.$dev[$lg].'</h2>';   
-	if( $_GET["pg"]=="home_")echo '<h2>'.'Mission Save'.'</h2>';     
-?>
+<div id="nexttotop"  align="center"  style="padding-left:10px; padding-right:10px;">
+<script type="text/javascript">
+function onresizeFunction(){
+   	var clientHeight = document.getElementById("topheaderid").clientHeight; 
+	document.getElementById('nexttotop').style.paddingTop = clientHeight+'px' ;
+}
+   	onresizeFunction();
+</script>
+<?php echo '<h2>'.@$stitle.'</h2>'; ?>
  </div>
  
  
  
-<div  align="center" class="htr" <?php 	if( $_GET["pg"]=="plan_") echo 'style="min-width:500px"'; ?> >
-
- <?php 
-
-$file= $_GET["pg"].$dlg[$lg].".xhtml";
-if( $_GET["pg"]=="fund"){
-	require	"fund.php";
-	$file="";
-}
-if($file!=""){
+<div  align="center" class="htr" <?php 	if( $_GET["pg"]=="plan") echo 'style="min-width:500px"'; ?> >
+<?php //filter htmltext
+function filterHtmlShow($file){
 	$filestr = file_get_contents($file, true);
 
 	$filestr = str_replace('<?xml version="1.0" encoding="UTF-8"?>','',$filestr);
 	
+	//home page center image
+	$filestr = str_replace('height:6.666cm;width:8.888cm; padding:0;  float:left;','',$filestr);
+	
 	$filestr = str_replace('margin-left:2cm; margin-right:2cm;','',$filestr);
 	
-	$filestr = str_replace('font-size:x-small','',$filestr);
+	$filestr = str_replace('<html>','',$filestr);
+	$filestr = str_replace('</html>','',$filestr);
+	
+	$filestr = str_replace('<body>','',$filestr);
+	$filestr = str_replace('</body>','',$filestr);
+	
+	//Calc alinha ao centro
+	$filestr = str_replace('font-size:x-small','margin-left: auto; margin-right: auto;',$filestr);
 	
 	$filestr = str_replace('* { margin:0;}','',$filestr);
 
 	$filestr = str_replace('<a href="http://www.youtube.com/embed/QZ-5ADFl6WQ" class="Internet_20_link">http://www.youtube.com/embed/QZ-5ADFl6WQ</a>','<iframe title="YouTube video player" class="youtube-player" type="text/html" width="100%" height="390" src="http://www.youtube.com/embed/QZ-5ADFl6WQ" frameborder="0" allowFullScreen></iframe>',$filestr);
 
 	$filestr = str_replace('position:relative;','',$filestr);
-	$filestr = str_replace('$doar','<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="danielchanfana@gmail.com"><input type="hidden" name="lc" value="PT"><input type="hidden" name="item_name" value="Foundation Mission Save"><input type="hidden" name="no_note" value="0"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest"><input type="image" src="https://www.paypalobjects.com/pt_PT/PT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - A forma mais fácil e segura de efetuar pagamentos online!"><img alt="" border="0" src="https://www.paypalobjects.com/pt_PT/i/scr/pixel.gif" width="1" height="1"></form>',$filestr);
-	$filestr = str_replace('$donate','<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="danielchanfana@gmail.com"><input type="hidden" name="lc" value="US"><input type="hidden" name="item_name" value="Foundation Mission Save"><input type="hidden" name="no_note" value="0"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',$filestr);
+	$filestr = str_replace('$doar','<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="danielchanfana@gmail.com"><input type="hidden" name="lc" value="PT"><input type="hidden" name="item_name" value="Mission Save"><input type="hidden" name="no_note" value="0"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest"><input type="image" src="https://www.paypalobjects.com/pt_PT/PT/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - A forma mais fácil e segura de efetuar pagamentos online!"><img alt="" border="0" src="https://www.paypalobjects.com/pt_PT/i/scr/pixel.gif" width="1" height="1"></form>',$filestr);
+	$filestr = str_replace('$donate','<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_donations"><input type="hidden" name="business" value="danielchanfana@gmail.com"><input type="hidden" name="lc" value="US"><input type="hidden" name="item_name" value="Mission Save"><input type="hidden" name="no_note" value="0"><input type="hidden" name="currency_code" value="EUR"><input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHostedGuest"><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>',$filestr);
 	echo $filestr; 
+	
 }
+?>
+ <?php 
+
+$file= $_GET["pg"]."_".$dlg[$lg].".xhtml";
+if(!file_exists($file))$file= "home_".$dlg[$lg].".xhtml";
+
+if( $_GET["pg"]=="fund"){
+	require	"fund.php";
+	$file="";
+}
+if($file!=""){
+	filterHtmlShow($file);
+}
+
+if( $_GET["pg"]=="jobs"){
+	require "jobs.php";
+	filterHtmlShow("jobsfoot_".$dlg[$lg].".xhtml");
+}
+if( $_GET["pg"]=="home"){ 
+	echo '<br><br>';
+	filterHtmlShow("nutri_".$dlg[$lg].".xhtml");
+}
+
+//https://developers.facebook.com/tools/debug/sharing/?q=http%3A%2F%2Fwww.betterplanetmission.com%2F%3Fl%3D0
+//<meta property="og:description" content=" ">
 ?>
 
 </div>
@@ -305,12 +358,12 @@ function dropdownFunction() {
   document.getElementById(arguments[0]).classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+// Close the dropdown menu if the user clicks outside of it, nao funciona n sei porque
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.dropbtn')) { 
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (i = 0; i < dropdowns.length; i++) { 
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
@@ -326,5 +379,6 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
 </script>
 </html>
